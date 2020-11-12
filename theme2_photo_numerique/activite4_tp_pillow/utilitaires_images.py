d7ca474622747c71277593fee4b8a4ef3b988148
python-villon <julien.de-villele@ac-orleans-tours.fr>
2020-11-12 22:07:28 +0100

    Initial commit

---
+++ theme2_photo_numerique/activite4_tp_pillow/utilitaires_images.py
@@ -0,0 +1,26 @@
+from PIL import Image
+import requests
+from io import BytesIO
+
+def zoomer(image, zoom):
+    if type(zoom) != int:
+        print('Attention : votre facteur de zoom doit être un nombre entier positif')
+    taille = image.size
+    taille_z = (taille[0] * zoom, taille[1] * zoom)
+    if taille_z[0] < 3000 and taille_z[1] < 3000:
+        if image.mode != "RGB":
+            print("display_zoom ne fonctionne qu'en mode RGB")
+        else :
+            image_z = Image.new("RGB", taille_z, (0,0,0))
+            tab = image.load()
+            tab_z = image_z.load()
+            for x in range(taille_z[0]):
+                for y in range(taille_z[1]):
+                    tab_z[x,y] = tab[x//zoom, y//zoom]
+            return image_z
+    else: 
+        print("Vous avez exagéré : votre affichage sera trop grand ! Réduire votre zomme, merci :-)")
+
+def ouvrir_image_par_url(url):
+    return Image.open(BytesIO(requests.get(url).content))
+        
