b95f99122eef484cdb454bc62dacbf0189c855c2
python-villon <julien.de-villele@ac-orleans-tours.fr>
2020-11-13 14:14:13 +0100

    MAJ utilitaires_images

--- theme2_photo_numerique/activite4_tp_champion_pillow/utilitaires_images.py
+++ theme2_photo_numerique/activite4_tp_champion_pillow/utilitaires_images.py
@@ -8,9 +8,9 @@ def zoomer(image, zoom):
     taille = image.size
     taille_z = (taille[0] * zoom, taille[1] * zoom)
     if taille_z[0] < 3000 and taille_z[1] < 3000:
-        if image.mode != "RGB":
-            print("display_zoom ne fonctionne qu'en mode RGB")
-        else :
+        if image.mode != "RGB" and image.mode != "L":
+            print("display_zoom ne fonctionne qu'en mode RGB ou en mode L")
+        elif image.mode == "RGB":
             image_z = Image.new("RGB", taille_z, (0,0,0))
             tab = image.load()
             tab_z = image_z.load()
@@ -18,6 +18,14 @@ def zoomer(image, zoom):
                 for y in range(taille_z[1]):
                     tab_z[x,y] = tab[x//zoom, y//zoom]
             return image_z
+        elif image.mode == "L":
+            image_z = Image.new("L", taille_z, 0)
+            tab = image.load()
+            tab_z = image_z.load()
+            for x in range(taille_z[0]):
+                for y in range(taille_z[1]):
+                    tab_z[x,y] = tab[x//zoom, y//zoom]
+            return image_z        
     else: 
         print("Vous avez exagéré : votre affichage sera trop grand ! Réduire votre zomme, merci :-)")
 
